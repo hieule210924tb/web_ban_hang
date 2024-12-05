d1d3d75882b2e6fb76d4ae403c16803e44630e22
hieu le 17 <manu210924@gmail.com>
2024-12-05 21:29:26 +0700

    html , css

--- trang_chu.php
+++ trang_chu.php
@@ -5,45 +5,39 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Trang chủ</title>
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css">
 </head>
 <body>
-    <?php  require "./connect.php"; ?>
-    <div class="navbar">
-        <ul class="menu">
-            <li><a href="#">Danh mục sản phẩm</a>
-                <?php
-                $sql = "SELECT * FROM `danhmuc_sp` " ;
-                $result= $conn->query($sql);
-                if($result-> num_rows>0){
-                for($i=0;$i<$result ->num_rows;$i++){
-                $row = $result->fetch_assoc();
-                echo "<p class='name_sp'>"
-                        .$row['name'].
-                    "</p>" ;
-                }
-                }
-            ?>
-            </li>
-            <li><a href="#">Sản phẩm</a>
-            <?php
-                $sql = "SELECT name, price, image FROM `san_pham` ";
-                $result = $conn->query($sql);
-                if ($result->num_rows > 0) {
-                 for($i=0;$i<$result ->num_rows;$i++) {
-                    $row = $result->fetch_assoc();
-                    echo "<div class='container'>";
-                    echo "<div class='product'>";
-                    echo "<div class='product-name'>"  .$row['name']. "</div>";
-                    echo "<div class='produce-img'><img src='./img/". $row['image'] . "'></div>";
-                    echo "<div class='product-price'>" .'Giá: ' .$row['price'] . "</div>";
-                    echo "</div>";
-                    echo "</div";
-                }
-                }
-                ?>
-
-        </li>
-            <li><a href="#">Quản lý sản phẩm</a></li>
+<header>
+        <div class="logo">Logo</div>
+        <div class="search">
+            <input type="text" placeholder="Tìm kiếm ...">
+            <i class="fa-solid fa-magnifying-glass"></i>
+        </div>
+        <div class="form-login">
+               <i class="fa-regular fa-user"></i>
+               <div class="login">
+                  <a href="#">Đăng kí</a>/
+                  <a href="#">Đăng nhập</a>
+               </div>
+        </div>
+    </header>
+    <div class="container">
+        <div class="container_left">
+            <h3>DANH MỤC SẢN PHẨM</h3>
+            <ul class="left-list">
+                <li class="left-item"><a href="#">Điện thoại</a></li>
+                <li class="left-item"><a href="#">Laptop</a></li>
+                <li class="left-item"><a href="#">Phụ kiện</a></li>
+            </ul>
+        </div>
+        <div class="container_right">
+             <ul class="right-list">
+                <li class="right-item"><a href="#">GIỚI THIỆU</a></li>
+                <li class="right-item"><a href="#">TRANG CHỦ</a></li>
+                <li class="right-item"><a href="#">TIN TỨC</a></li>
+             </ul>
+        </div>
     </div>
 </body>
 </html>
